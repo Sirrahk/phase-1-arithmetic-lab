bf404077006c7d454b2ab6091468b72a08cdcf6c
Keara Harris <keara.s.harris@gmail.com>
2023-04-03 15:06:39 -0500

    Completed arithmetic-lab using multiply, Math.random, modulo, and Math.max

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+//Multiply (*)
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply)
 
-// Write your code here
+
+
+//Random (Math.random())
+let random = Math.floor(Math.random() * 10) + 1;
+ console.log(random)
+
+//Mod (%: returns the remainder )
+let num3 = 22;
+let num4 = 9; 
+const mod = num3 % num4;
+console.log(mod)
+
+//Max
+const max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
